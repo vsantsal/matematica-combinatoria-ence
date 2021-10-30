9b71da28fab547d2405e7d1b021d361024d98b4b
vsantsal <vsantsal@gmail.com>
2021-10-30 19:28:27 -0300

    Refatorando exercício 24 da aula 01, separando a rotina para função, caso seja útil em prova/exercícios futuoros.

--- exercicios/aula_01.py
+++ exercicios/aula_01.py
@@ -1,5 +1,26 @@
 from itertools import product
 import numpy as np
+from typing import (
+    Dict,
+    List,
+)
+
+
+def retorna_produtos_de_dicio_de_bases_e_expoentes(dicio: Dict[int, range]) -> List:
+    """
+    Função que pode ser útil para resolver exercícios da matéria.
+
+    Passar como input dicionário em que a chave são bases (fatores primos ou não),
+    e seus valores são range com expoentes possíveis.
+            
+    :param dicio:
+    :return:
+    """
+    expoentes = np.array([*product(*dicio.values())])
+    bases_produtos = np.array([*dicio.keys()])
+
+    produtos = [np.prod(bases_produtos ** expoente) for expoente in expoentes]
+    return produtos
 
 
 # Exercício 24 da lista
@@ -8,10 +29,7 @@ dados_enunciado = {5: range(2),
                    7: range(3),
                    9: range(4)}
 
-expoentes = np.array([*product(*dados_enunciado.values())])
-bases_produtos = np.array([*dados_enunciado.keys()])
-
-produtos_possiveis = [np.prod(bases_produtos**expoente) for expoente in expoentes]
+produtos_possiveis = retorna_produtos_de_dicio_de_bases_e_expoentes(dados_enunciado)
 produtos_possiveis.sort()
 
 print(len(produtos_possiveis))
