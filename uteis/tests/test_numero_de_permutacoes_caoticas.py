62fe281551645bcb4a418b4820c91dd8c63c9334
vsantsal <vsantsal@gmail.com>
2021-12-04 12:53:54 -0300

    Adicionando função para obter número de permutações caóticas, com alguns elementos em posições originais (fixos).

--- uteis/tests/test_numero_de_permutacoes_caoticas.py
+++ uteis/tests/test_numero_de_permutacoes_caoticas.py
@@ -1,5 +1,8 @@
 import pytest
-from uteis.permutacoes import numero_de_permutacoes_caoticas
+from uteis.permutacoes import (
+    numero_de_permutacoes_caoticas,
+    numero_de_permutacoes_caoticas_com_elementos_fixos,
+)
 
 
 @pytest.mark.parametrize('inteiro, perms_esperadas',
@@ -8,4 +11,14 @@ from uteis.permutacoes import numero_de_permutacoes_caoticas
                           (15, 481066515734)])
 def test_resultados_basicos_de_numero_de_permutacoes_caoticas(inteiro, perms_esperadas):
     perms_obtidas: int = numero_de_permutacoes_caoticas(inteiro)
+    assert perms_obtidas == perms_esperadas
+
+
+@pytest.mark.parametrize('els, fixos, perms_esperadas',
+                         [(10, 4, 55650),
+                          (10, 2, 667485), ])
+def test_resultados_basicos_de_numero_de_permutacoes_caoticas_com_elementos_fixos(els,
+                                                                                  fixos,
+                                                                                  perms_esperadas):
+    perms_obtidas: int = numero_de_permutacoes_caoticas_com_elementos_fixos(els, fixos)
     assert perms_obtidas == perms_esperadas
